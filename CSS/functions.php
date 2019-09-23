14f57deb4b7ca29615cca1c77ffa6665a4ffedc6
Romain Leroy <r.leroy@codeur.online>
2019-09-23 16:45:42 +0200

    ajout de la date et heure d'un post

---
+++ CSS/functions.php
@@ -0,0 +1,68 @@
+<?php require TEMPLATEPATH.'/framework/theme.php';
+$theme = new Theme(array(
+  'menus' => array(
+    'nav' => 'Navigation'
+  )
+));
+
+add_theme_support( 'post-thumbnails' );
+
+function lambda_styles() {
+  wp_register_style('normalize-styles', 'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css', array(), true);
+  wp_enqueue_style('normalize-styles');
+  wp_register_style('bootstrap-style', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), true);
+  wp_enqueue_style('bootstrap-style');
+  wp_register_style('main-style', get_template_directory_uri().'/main.css', array(), true);
+  wp_enqueue_style('main-style');
+}
+add_action( 'wp_enqueue_scripts', 'lambda_styles' );
+
+function lambda_scripts() {
+  wp_register_script('fontawesome-script', 'https://kit.fontawesome.com/6fee70888d.js', array(), true);
+  wp_enqueue_script('fontawesome-script');
+  wp_register_script('jquery-script', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), true);
+  wp_enqueue_script('jquery-script');
+  wp_register_script('popper-script', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', array(), true);
+  wp_enqueue_script('popper-script');
+  wp_register_script('bootstrap-script', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array(), true);
+  wp_enqueue_script('bootstrap-script');
+  wp_register_script('main-script', get_template_directory_uri().'JS/main.js', array('masonry-script'), true);
+  wp_enqueue_script('main-script');
+}
+add_action( 'wp_footer', 'lambda_scripts' );
+
+function add_Main_Nav() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'add_Main_Nav' );
+
+function my_custom_init(){
+  register_post_type(
+    'picture',
+    array(
+      'label' => 'Pictures',
+      'labels' => array(
+        'name' => 'Pictures',
+        'singular_name' => 'Picture',
+        'all_items' => 'Toutes les images',
+        'add_new_item' => 'Ajouter une image',
+        'edit_item' => 'Éditer l\'image',
+        'new_item' => 'Nouvelle immage',
+        'view_item' => 'Voir l\'image',
+        'search_items' => 'Rechercher parmi les images',
+        'not_found' => 'Pas d\'image trouvée',
+        'not_found_in_trash'=> 'Pas d\'image dans la corbeille'
+        ),
+      'public' => true,
+      'capability_type' => 'post',
+      'supports' => array(
+        'title',
+        'thumbnail'
+      ),
+      'has_archive' => true
+    )
+  );
+}
+add_action('init', 'my_custom_init');
+
+?>
---
+++ CSS/header.php
@@ -0,0 +1,18 @@
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="author" lang="fr" content="Eddy MORLON, Thomas Gallusser">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?> &raquo; <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
+    <link rel="stylesheet" href="progressBar.css">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+  	<link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
+  	<?php endif; ?>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
